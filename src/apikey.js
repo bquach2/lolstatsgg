955ffc53daa59c94cc8ddff08413befc5108ea5b
Brandon Quach <bquach@umich.edu>
2022-08-19 17:31:14 -0400

    Hid api key

--- src/apikey.js
+++ src/apikey.js
@@ -1,3 +1,4 @@
-let apikey = "RGAPI-3c73131d-933b-4e64-9970-cc19d16c28d3"
+// Add in personal Riot Developer API KEY here
+let apikey = "RGAPI-3c73131d-933b-4e64-9970-XXXXXXXXXXXX"
 
 export default apikey
